4becbd8a367fc94401e60576f5487c1fdd401091
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-11-08 22:51:23 -0500

    se agregaron imagenes, practica

--- ClonInstagram/urls.py
+++ ClonInstagram/urls.py
@@ -1,11 +1,14 @@
 #url
 from django.urls import path
 
-from ClonInstagram import views
+from ClonInstagram import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world),
-    path("sorted/", views.sorted_numers),
-    path("saludo/<str:name>/<int:age>", views.hi),
-    path("hola/<str:nombre>/<int:edad>",views.saludo)
+    path('hello-world/', local_views.hello_world),
+    path("sorted/", local_views.sorted_numers),
+    path("saludo/<str:name>/<int:age>", local_views.hi),
+    path("hola/<str:nombre>/<int:edad>", local_views.saludo),
+
+    path("posts/", posts_views.list_pos),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from datetime import datetime
+
+posts = [
+	{
+		"name": "Joel Cedeño",
+		"user": "Jioska",
+		"timestamp": datetime.now().strftime("%b %dth %Y - %H:%M hrs"),
+		"picture": "https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE",
+	},
+	{
+		"name": "Aksoij",
+		"user": "Idontknow",
+		"timestamp": datetime.now().strftime("%b %dth %Y - %H:%M hrs"),
+		"picture": "https://i.picsum.photos/id/989/200/200.jpg?hmac=YmaagsSArKDGDSeyRJ9aYRxKM6KdP49ZGYtyhLHlCcY",
+	},
+	{
+		"name": "Karla",
+		"user": "Karla007",
+		"timestamp": datetime.now().strftime("%b %dth %Y - %H:%M hrs"),
+		"picture": "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
+	}
+]
+
+def list_pos(request):
+	"""lista de posts existentes"""
+	content = []
+	for post in posts:
+		content.append("""
+			<p><strong>{name}</strong></p>
+			<p><small>{user} - <i>{timestamp}</i></small></p>
+			<figure><img src="{picture}"/></figure>
+		""".format(**post))
+	return HttpResponse("<br>".join(content))
